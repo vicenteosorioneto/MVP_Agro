6a2c10046f8ad7f7f47c374fe19f57c8b5e46b37
vicenteosorioneto <osoriocosta8@gmail.com>
2026-05-11 15:18:35 -0300

    .env

--- public/service/api.js
+++ public/service/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = `http://${window.location.hostname}:5000/api`;
 
 (function clearLegacyKeys() {
   localStorage.removeItem('accessToken');
